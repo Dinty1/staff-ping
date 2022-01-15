47c74bb52e6bf317efcebffd8eb8dbf4a272e0fa
Dinty1 <67452089+Dinty1@users.noreply.github.com>
2022-01-15 13:03:45 +0000

    update staff

--- config/config.js
+++ config/config.js
@@ -9,21 +9,21 @@ module.exports = {
     server: 'minecartrapidtransit.net',
     lastStaffSeenMessage: '812103142333349909',
     staff: [
-        'd2938ef4-3044-4a6d-8997-c635f643153c',//echohue
         'b5f4e511-d13d-4c93-931b-c526a68c7c74',//skelezomperman
         '125df18c-516d-4bfe-bb04-88254ed25d00',//tom_pairs
         '6f4821b0-1996-4f3b-a776-ce0cdc8e7276',//ap_red
         'f65bc7cb-ce43-477c-baf7-1b4c72798bd0',//chiefbozx
         '7c385c95-195e-4576-8771-ec4f6f00dc57',//cynra_
-        '5cc70692-7282-4fd5-8d89-11c08535bb11',//sesese9
         'ffdaf900-cdb2-4f09-a0fb-81e3087da4e7',//frumple
         '49590c1d-3fb8-4f6c-9bdd-39351621c679',//needn_nl
         '9d623930-b069-4cb5-94e4-81499eb2cb92',//missa_solemnis
         '472715f5-e298-428c-961f-9b60dbe9dbad',//dint e
         'fe400b78-b441-4551-8ede-a1295434a13b',//hvt
-        'b080e6f8-9a4d-46d2-8c9e-ec3c6e8b2371',//ondist
         'e490b27a-d51d-4c59-b277-8cc09c3c4178',//protocol
-        'bbab1811-6079-44b0-aeb2-ad1f3841168f',//narn
         '09805c69-6a39-4047-8438-5cece9372d95',//mpolo
+        '7e96f1a3-d9be-4ca8-a2ac-a67f49c6095e',//7d
+        '16990807-9df4-4bde-89b7-efee9836b7a6',//moj
+        'e7772c67-a5a7-4b10-81d1-304025d75880',//simon
+        '4ab3e8f8-d259-4f36-ad72-78a756afbf1c'//vicki
     ]
 }
