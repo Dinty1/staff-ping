66130aa05415c3bcb28c8df88197d608aa1ac060
Dinty1 <dintybmrt@gmail.com>
2022-07-17 23:30:32 +0100

    Full emoji names

--- src/ServerMonitor.js
+++ src/ServerMonitor.js
@@ -185,9 +185,9 @@ export default class ServerMonitor {
 
     rankEmoji(rank) {
         switch (rank) {
-            case "Admin": return "<:Ad:997944788747825284>";
-            case "Mod": return "<:Mo:997944804518395994>";
-            case "Conductor": return "<:Co:997944814295334913>";
+            case "Admin": return "<:Admin:997944788747825284>";
+            case "Mod": return "<:Mod:997944804518395994>";
+            case "Conductor": return "<:Conductor:997944814295334913>";
         }
     }
 }
