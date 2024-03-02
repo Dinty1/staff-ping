a889a34f0767edc733b62a636775b998b5f82ec1
Dinty1 <dintybmrt@gmail.com>
2024-03-02 20:42:38 +0000

    Maybe fix leave messages

--- src/LeaveListener.js
+++ src/LeaveListener.js
@@ -2,6 +2,7 @@ import { config } from "./index.js"
 
 export default class LeaveListener {
     constructor(client) {
+        client.guilds.cache.get(config.guild).members.fetch();
         client.on("guildMemberRemove", member => {
             client.channels.cache.get(config.general_channel).send(`:red_circle: ${member.user} left.`)
         })
