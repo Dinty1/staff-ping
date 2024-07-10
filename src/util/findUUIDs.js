a2ae97bdad073c829f3f137b507dce59160da4bc
Dinty1 <dintybmrt@gmail.com>
2024-07-10 11:15:06 +0100

    more useful error when post fails

--- src/util/findUUIDs.js
+++ src/util/findUUIDs.js
@@ -3,7 +3,9 @@ export default async function findUUIDs(names) {
     let output = [];
     for (let i = 0; i < names.length; i += 10) {
         let { data } = await axios.post("https://api.mojang.com/profiles/minecraft", names.slice(i, i + 10))
-            .catch(error => { throw new Error("Unable to reach Mojang API") });
+            .catch(error => { 
+                throw error;
+             });
         if (data) output.push(...data.map(p => p.id));
     }
     return output;
